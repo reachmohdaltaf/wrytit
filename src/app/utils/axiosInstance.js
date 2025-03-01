1c426789ab565fadd48c276c81dca5e5e5e7d7d1
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-03-01 10:46:57 +0530

    first commit

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", // Set this in .env.local
+  baseURL: "/api", // Set this in .env.local
   withCredentials: true,
 });
 
