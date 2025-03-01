d6fd5f528b8b7230d7a4e7aa0888494f4a3a9a94
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-03-01 10:52:20 +0530

    firs2

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api", // Set this in .env.local
+  baseURL: "https://wrytit.vercel.app/api", // Set this in .env.local
   withCredentials: true,
 });
 export default axiosInstance;
